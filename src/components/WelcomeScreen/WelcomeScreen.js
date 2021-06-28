86076a3ce8da3837b5f7930e755e5994169a4dbc
Damian <wojtunikdw@gmail.com>
2021-06-28 20:52:10 +0200

    WIP Updates to UI, blob animation

--- src/components/WelcomeScreen/WelcomeScreen.js
+++ src/components/WelcomeScreen/WelcomeScreen.js
@@ -17,7 +17,15 @@ const WelcomeScreen = () => {
                 <p className={styles.CharacteristicLine}>▸ Artificial neural network applied</p>
                 <button className={styles.GradientButton}>Try it now</button>
             </div>
-            <GraphIcon className={styles.WelcomeGraphic} />
+            <svg viewBox="0 0 480 480" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="100%" className={styles.lgBlob}>
+                <defs>
+                    <linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">
+                        <stop offset="0%" style={{stopColor: "rgb(20, 125, 190)"}}></stop>
+                        <stop offset="100%" style={{stopColor: "rgb(29, 39, 73)"}}></stop>
+                    </linearGradient>
+                </defs>
+                <path id="blob" d="M389.5,297Q418,354,365,376Q312,398,260.5,427Q209,456,155.5,427.5Q102,399,65,350.5Q28,302,51.5,246.5Q75,191,95,143.5Q115,96,162.5,64Q210,32,247.5,86Q285,140,336,142.5Q387,145,374,192.5Q361,240,389.5,297Z" fill="url(#gradient)"></path>
+            </svg>
         </div>
     )
 }
