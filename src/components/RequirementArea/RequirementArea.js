bae170c8d0c129c3b6b072353df1f0329ebbfecb
Damian <wojtunikdw@gmail.com>
2021-08-20 19:05:48 +0100

    URL change

--- src/components/RequirementArea/RequirementArea.js
+++ src/components/RequirementArea/RequirementArea.js
@@ -38,7 +38,7 @@ const RequirementArea = () => {
         const data = { email: email, stories: JSON.stringify(userStories), token: token };
 
         axios
-            .post("http://localhost:3333/api/stories", {...data})
+            .post("https://requml-node.herokuapp.com/api/stories", {...data})
             .then(res => {
                 if(res.status === 200) {
                     setSpinnerVisible(false);
